d641b2f4dc6c90a13868f081f803ef0fc6eadbf5
omer mohamed <damouramory@gmail.com>
2025-10-18 00:04:27 +0300

    testing: resetTimer functinoality

--- src/navigation/App.Navigator.tsx
+++ src/navigation/App.Navigator.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/Login/Login.Screen';
 import MainNavigator from './Main.Navigator';
+import { useInactivityTimer } from '../hooks/useInactivityTimer';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
+  const { resetTimer } = useInactivityTimer();
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Main" component={MainNavigator} />
+      <Stack.Screen
+        name="Main"
+        component={() => <MainNavigator resetTimer={resetTimer} />}
+      />
     </Stack.Navigator>
   );
 }
