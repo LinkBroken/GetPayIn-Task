26cc0418efe3f50577d74899f81c6bebcafebdc4
omer mohamed <damouramory@gmail.com>
2025-10-18 00:00:34 +0300

    feat: add eslint rules for dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  rules: {
+    'react/display-name': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 };
